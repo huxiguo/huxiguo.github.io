7aa494abfc1baec835f91f70ed44502c2c700087
michelhu <michelhu@tencent.com>
2022-09-02 10:50:58 +0800

    update github action

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base:'/huxg/',
+  // base:'/huxg/',
   lang: 'zh-CN',
   title: 'huxg的笔记',
   description: '这是我的第一个 VuePress 站点',
+  // pagePatterns: ['**/*.md', '!.vuepress', '!node_modules', '!dist'],
 })
