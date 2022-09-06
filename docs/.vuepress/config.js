179d70a54eb0509a30263ea175506c18a308c2eb
SeeGreatHu <hxg5211217@qq.com>
2022-09-06 19:02:25 +0800

    add navbar&sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,33 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  // base:'/huxg/',
+import { defaultTheme } from 'vuepress'
+export default {
   lang: 'zh-CN',
   title: 'huxg的笔记',
   description: '这是我的第一个 VuePress 站点',
-  // pagePatterns: ['**/*.md', '!.vuepress', '!node_modules', '!dist'],
-})
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com'
+      }
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: 'Linux',
+          collapsible: true,
+          children: ['/command/linux_command.md']
+        },
+        {
+          text: 'ES6',
+          collapsible: true,
+          children: ['/guide/guide1.md']
+        }
+      ]
+    }
+  })
+}
