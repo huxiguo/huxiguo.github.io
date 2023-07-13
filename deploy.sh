7ffe2bc5034b1674e3122eceb2dd8aa69c5b4e8c
huxiguo <hu@seekhoo.cn>
2023-07-13 11:26:11 +0800

    add yarn lock

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/huxiguo/huxiguo.github.io.git main:blogs
+git push -f https://github.com/huxiguo/docs.git
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
